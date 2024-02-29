ef6306919386ea7a59f0806cdabc589943b9a418
Efe Han Oz <159789584+wlanHan@users.noreply.github.com>
2024-02-29 20:21:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+const paragraphs = [
+    "Hangi tür kariyerin sizin için en iyi olabileceğini düşünürken akıllıca ve iyi bir seçim yapmaya çalışmak zor bir iştir. Geleceğiniz, iş dünyasında sizin için en iyi açık pozisyonları bulma konusunda izleyeceğiniz yollara bağlı olabilir. Bazı insanlar dünyada kendileri için tek bir işin olduğunu hissedecekler. Çok düşünmek ve çok çalışmak, kendileri için en iyi işi bulmalarına yardımcı olacaktır. İşler, becerilere ve iyi bir iş ahlakına sahip olanlar için vardır. Yukarı New England eyaletlerindeki birçok yeni genç sanatçı, yeni Amerikan sanatının liderleri olarak dünya çapında ünlüdür. Bu iyi sanatçılar seçtikleri alanlarda çok iyidirler ve birçok yeteneklerini başkalarına öğreterek paylaşmaya isteklidirler. Öğrenciler yağlıboya, tebeşirle eskiz, heykel ve dokuma konularında bilgi edinme ve kullanma fırsatı buldular. Daktilo yazmayı öğrenmek, bugünkü çalışmalarımızda hepimize yardımcı olacak bir beceridir.",
+    "Bir satış elemanını tanımlamak için kullanılan iki yaygın terim Çiftçi ve Avcı'dır. Gerçek şu ki çoğu profesyonel satış elemanı her ikisinden de biraz var. Bir avcı genellikle agresif satış tekniklerini kullanan agresif kişiliklerle ilişkilendirilir. Satış metodolojisi açısından avcı, odak noktası anlaşmaları yapmak ve kapatmak olan kişiyi ifade eder. Bu sürece satış yakalama denir. Bir örnek, uzun mesafeli bir satış elemanı, ayakkabı satıcısı ve bir dereceye kadar araba satıcısı gibi bir emtia satışıdır. Görevleri alıcıları bulmak ve dönüştürmektir. Satış çiftçisi, satın alma sürecini doğrudan etkileyen ve değiştiren faaliyetler yoluyla satış talebi yaratan kişidir.",
+    "Jefferson Lisesi'nin 1984 sınıfından iki üye, 20 yıllık sınıf buluşması için tatil yeri arayan 18 kişilik bir gruba başkanlık ediyor. Şehirden 78 mil uzakta güzel bir yerin en iyisi olduğu ortaya çıktı. 254 odası ve 378 kişilik bir ziyafet salonu vardır. 30 Mayıs 1926'daki açılışından bu yana yılın 365 günü açıktır. Tesisi rezerve etmek için 450 güne ihtiyaçları olacak. Debbie Holmes, tüm firma için 2.847 ofis makinesi satın almakla görevlendirildi. Debbie 6 ayda 35 eyalette 109'dan fazla şirketi ziyaret etti. Bugün saat 17.00'de 2784 numaralı odada yönetim kuruluna rapor verecek. Kurul, bu 109 firma hakkındaki raporu değerlendirecek ve satın alınması gereken en iyi 2 veya 3 markayı önerecek. Debbie'nin 16 Ağustos'tan önce karar vermesi gerekiyor. Lynn Greene, proje üzerinde çalışmaların 27 Mart 2003'te başladığını söyledi. 246 plan, 18 gün önce ofise postayla gönderildi. Baskıların kabul edilebilir hale gelmesi için yüzde 100 doğru olması gerekiyordu. Projenin 31 Mayıs 2025'e kadar tamamlanması gerekiyor. O tarihte her biri en az 16 odalı 47 yeni apartman olacak. Bina 25 katlı olacak.",
+    "Bir sabah arkadaşım ve ben yaz tatilimizi okuldan uzakta nasıl planlayabileceğimizi düşünüyorduk. Kendi eyaletimizden yakınlardaki birkaç eyalete gitmek, sınırlı fonlarımızı genişletmemize yardımcı olacaktır. Altı arkadaşımızı daha bize eşlik etmek üzere davet etmek, araç masraflarımızı azaltır. Belirli bölgelerde durmak, gerçekten sınırlı olan seyahat bütçemizi genişletmemize de yardımcı olacaktır. Dün finansla ilgili ilginç ve aydınlatıcı bir tartışmaya katıldım. Hayatım boyunca en az yarım milyon dolar kazanabileceğimi hayal etmekte zorlanıyordum. Aynı dönemde yarım milyon kadar harcama yapmam da mümkün. Benim için asıl zor olan şey, harcadığımdan yarım milyonun fazlasını biriktirmek olacak.",
+    "Serbest çalışan veya serbest çalışan, serbest meslek sahibi olan ve uzun vadede belirli bir işverene bağlı olması gerekmeyen bir kişi için yaygın olarak kullanılan bir terimdir. Serbest çalışanlar bazen müşterilere serbest emek satan bir şirket veya geçici bir kurum tarafından temsil edilir; diğerleri bağımsız olarak çalışır veya iş bulmak için profesyonel dernekleri veya web sitelerini kullanır. Bağımsız yüklenici terimi, İngilizce'nin daha yüksek bir sicilinde bu tür çalışanların vergi ve istihdam sınıflarını belirtmek için kullanılacak olsa da, serbest çalışma terimi kültür ve yaratıcı endüstrilerde en yaygın olanıdır ve bu terim özellikle bunlara katılım önergelerini verir. Serbest çalışmanın baskın olduğu alanlar, meslekler ve endüstriler şunları içerir: müzik, yazarlık, oyunculuk, bilgisayar programlama, web tasarımı, grafik tasarım, çeviri ve illüstrasyon, film ve video prodüksiyonu ve bazı kültür teorisyenlerinin, serbest çalışmanın merkezi olarak gördüğü diğer parça eser türleri. bilişsel-kültürel ekonomi.",
+    "Düzenleme, hizmet sektöründe giderek büyüyen bir çalışma alanıdır. Ücretli düzenleme hizmetleri, uzman düzenleme firmaları veya serbest çalışan (serbest) editörler tarafından sağlanabilir. Düzenleme firmaları, kurum içi editörlerden oluşan bir ekip çalıştırabilir, bireysel yüklenicilerden oluşan bir ağa güvenebilir veya her ikisini de yapabilir. Bu tür firmalar, bireysel editörlerin becerilerine bağlı olarak çok çeşitli konu ve türlerde düzenlemeyi gerçekleştirebilmektedir. Bu editörler tarafından sağlanan hizmetler çeşitli olabilir ve redaksiyon, kopya düzenleme, çevrimiçi düzenleme, gelişimsel düzenleme, arama motoru optimizasyonu (SEO) için düzenleme vb. içerebilir. Serbest çalışan editörler doğrudan müşteriler için çalışır veya hizmetlerini düzenleme firmaları aracılığıyla sunarlar. ya da her ikisi de. Bir düzenleme türünde ve belirli bir konu alanında uzmanlaşabilirler. Yazarlar için doğrudan çalışan ve onlarla profesyonel ilişkiler geliştiren kişilere yazar editörü denir.",
+    "Kartal Parmağı olarak da bilinen avla ve gagala (iki parmakla yazma), daktilonun her tuşa ayrı ayrı bastığı yaygın bir yazma biçimidir. Anahtarların hafızaya alınmış konumuna güvenmek yerine, daktilonun her anahtarı görerek bulması gerekir. Bu yöntemin kullanılması, daktilonun, gözlerini tuşlardan ayırmadan, yazılanları görmesini de engelleyebilir. İyi bir doğruluk elde edilse de, yapılan yazım hataları kullanıcının ekrana bakmaması nedeniyle hemen fark edilmeyebilir. Ayrıca daha az sayıda parmak kullanıldığı için kullanılanların çok daha büyük bir mesafe hareket etmeye zorlanması gibi bir dezavantaj da vardır.",
+    "Düzeltmen adayları öncelikle yazım, hız ve örnek metindeki hataları bulma becerileri açısından test edilir. Bu amaçla, onlara klasik olarak zor olan on veya yirmi kelimeden oluşan bir liste ve her ikisi de sıkı zamanlanmış bir düzeltme testi verilebilir. Düzeltme testinde genellikle metin miktarı başına maksimum sayıda hata ve bunları bulmak için minimum süre bulunur. Bu yaklaşımın amacı en iyi becerilere sahip olanları belirlemektir.",
+    "Mühendisliğin uygulayıcıları olarak mühendisler, pratiklik, düzenleme, güvenlik ve maliyetten kaynaklanan sınırlamaları göz önünde bulundurarak amaç ve gereksinimleri karşılamak için makineler, sistemler, yapılar ve malzemeleri icat eden, tasarlayan, analiz eden, inşa eden ve test eden kişilerdir. Mühendislerin çalışmaları, bilimsel keşifler ile bunların insan ve iş ihtiyaçlarına ve yaşam kalitesine yönelik daha sonraki uygulamaları arasındaki bağlantıyı oluşturur.",
+    "Dakikadaki kelime sayısı (WPM), işe alımda yaygın olarak kullanılan yazma hızının bir ölçüsüdür. WPM ölçümü amacıyla bir kelime, beş karaktere veya tuş vuruşuna göre standartlaştırılır. Bu nedenle, kahverengi tek kelime olarak sayılır, ancak hesaplanan iki kelime olarak sayılır. Giriş hızının standartlaştırılmış ölçümünün faydaları, dil ve donanım sınırları arasında karşılaştırma yapılmasına olanak sağlamasıdır. Cape Town'da Afrikanca konuşan bir operatörün hızı, Paris'te Fransızca konuşan bir operatörün hızıyla karşılaştırılabilir.",
+];
+
+const typingText = document.querySelector(".typing-text p");
+const inField = document.querySelector(".wrapper .input-field");
+const tryAgainBtn = document.querySelector(".content button");
+const timeTag = document.querySelector(".time span b");
+const mistakeTag = document.querySelector(".mistake span");
+const wpmTag = document.querySelector(".wpm span");
+const cpmTag = document.querySelector(".cpm span");
+
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = mistakes = isTyping = 0;
+
+function loadParagraph() {
+    const ranIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    paragraphs[ranIndex].split("").forEach(char => {
+        let span = `<span>${char}</span`
+        typingText.innerHTML += span;
+    });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+    document.addEventListener("keydown", () => inField.focus());
+    typingText.addEventListener("click", () => inField.focus());
+}
+
+function initTyping() {
+    let characters = typingText.querySelectorAll("span");
+    let typedChar = inField.value.split("")[charIndex];
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if (!isTyping) {
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
+        }
+        if (typedChar == null) {
+            if (charIndex > 0) {
+                charIndex--;
+                if (characters[charIndex].classList.contains("incorrect")) {
+                    mistakes--;
+                }
+                characters[charIndex].classList.remove("correct", "incorrect");
+            }
+        } else {
+            if (characters[charIndex].innerText == typedChar) {
+                characters[charIndex].classList.add("correct");
+            } else {
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+            }
+            charIndex++;
+        }
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
+
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
+
+        wpmTag.innerText = wpm;
+        mistakeTag.innerText = mistakes;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        clearInterval(timer);
+        inField.value = "";
+    }
+}
+
+function initTimer() {
+    if (timeLeft > 0) {
+        timeLeft--;
+        timeTag.innerText = timeLeft;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
+        wpmTag.innerText = wpm;
+    } else {
+        clearInterval(timer);
+    }
+}
+
+function resetGame() {
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    charIndex = mistakes = isTyping = 0;
+    inField.value = "";
+    timeTag.innerText = timeLeft;
+    wpmTag.innerText = 0;
+    mistakeTag.innerText = 0;
+    cpmTag.innerText = 0;
+}
+
+loadParagraph();
+inField.addEventListener("input", initTyping);
+tryAgainBtn.addEventListener("click", resetGame);
